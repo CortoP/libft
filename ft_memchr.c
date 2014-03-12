6dad289bf56f9b7ab08f548ed9a4153d502d6db6
CortoP <vlehuger@student.42.fr>
2014-03-12 11:54:39 +0100

    norme

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlehuger <vlehuger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/12 11:52:42 by vlehuger          #+#    #+#             */
+/*   Updated: 2014/03/12 11:54:08 by vlehuger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdlib.h>
 
-void	   *ft_memchr(const void *s, int c, size_t n)
+void			*ft_memchr(const void *s, int c, size_t n)
 {
-  size_t  i;
-  unsigned char	  *t;
-  
-  if (s == NULL)
-    return (NULL);
-  t = (unsigned char *)s;
-  i = 0;
-  while (i < n && t[i])
-    {
-      if (t[i] == (unsigned char)c)
-	return (t + i);
-      i++;
-    }
-  return (NULL);
+	size_t			i;
+	unsigned char	*t;
+
+	if (s == NULL)
+		return (NULL);
+	t = (unsigned char *)s;
+	i = 0;
+	while (i < n && t[i])
+	{
+		if (t[i] == (unsigned char)c)
+			return (t + i);
+		i++;
+	}
+	return (NULL);
 }
--- ft_memset.c
+++ ft_memset.c
@@ -1,14 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlehuger <vlehuger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/12 11:51:25 by vlehuger          #+#    #+#             */
+/*   Updated: 2014/03/12 11:52:01 by vlehuger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <string.h>
 
-void		*ft_memset(void *b, int c, size_t len)
+void				*ft_memset(void *b, int c, size_t len)
 {
-  size_t	i;
-  unsigned char	*s;
-  
-  s = (unsigned char *)b;
-  i = -1;
-  while (++i < len)
-    s[i] = (unsigned char)c;
-  return (b);
+	size_t			i;
+	unsigned char	*s;
+
+	s = (unsigned char *)b;
+	i = -1;
+	while (++i < len)
+		s[i] = (unsigned char)c;
+	return (b);
 }
